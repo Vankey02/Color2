662f9fa392d01ae48ab18e011e42486d66c7d8dd
Vankey02 <keciamamilus@gmail.com>
2024-03-05 22:36:31 +0100

    adding photoreader

---
+++ client/src/components/PhotoInput.jsx
@@ -0,0 +1,99 @@
+import { useContext, useState, useEffect, useRef } from "react";
+import QRCode from "qrcode";
+import { stagesContext } from "../helpers/context";
+import { motion } from "framer-motion";
+import motionvariants from "../helpers/motionVariants";
+import Quagga from "@ericblade/quagga2";
+
+export default function PhotoInput({ setImageError }) {
+  const [image, setImage] = useState();
+  const ref = useRef(null);
+
+  const { color, setDownload, setStage } = useContext(stagesContext);
+  useEffect(() => {
+    console.log("berofe quagga", image);
+    Quagga.decodeSingle(
+      {
+        decoder: {
+          readers: ["code_128_reader"], // List of active readers
+        },
+        locate: true, // try to locate the barcode in the image
+        src: image, // or 'data:image/jpg;base64,' + data
+      },
+      function (result) {
+        if (result != null) {
+          console.log("pass");
+          if (result.codeResult) {
+            console.log("result", result.codeResult.code);
+
+            if (result.codeResult.code.toString().length === 11) {
+              QRCode.toDataURL(result.codeResult.code)
+                .then((url) => {
+                  console.log("QRcodeURL", url);
+                  setDownload(url);
+                  setStage((prevStage) => prevStage + 1);
+                })
+                .catch((err) => {
+                  console.log("not detected");
+
+                  setImageError((val) => !val);
+                  console.error(err);
+                });
+            } else {
+              console.log("not detected");
+              setImageError((val) => !val);
+            }
+          } else {
+            console.log("not detected");
+            setImageError((val) => !val);
+          }
+        } else {
+          console.log("not detected");
+
+          setImageError((val) => !val);
+        }
+      }
+    );
+  }, [image]);
+  return (
+    <>
+      <input
+        ref={ref}
+        id="photoInput"
+        className="invisible "
+        type="file"
+        accept="image/*"
+        onChange={(e) => {
+          if (!e.target.files || e.target.files.length == 0) return;
+
+          const file = e.target.files[0];
+          console.log(file);
+          var reader = new FileReader();
+          reader.readAsDataURL(e.target.files[0]);
+          reader.onload = () => {
+            if (reader.result != null) {
+              console.log("input result", reader.result);
+              setImage(reader.result.toString());
+            } //error
+          };
+          //
+        }}
+      />
+      <motion.div
+        className="p-2 rounded-md cursor-pointer mb-14"
+        whileHover={{ scale: 1.1 }}
+        style={{
+          borderWidth: 2,
+          borderColor: `${color.color}`,
+          color: `${color.color}`,
+        }}
+        variants={motionvariants.itemTop}
+        onClick={() => {
+          document.getElementById("photoInput").click();
+        }}
+      >
+        I have a photo
+      </motion.div>
+    </>
+  );
+}
--- client/src/components/StageThree.jsx
+++ client/src/components/StageThree.jsx
@@ -8,27 +8,34 @@ import CodeInput from "./CodeInput";
 import ButtonNext from "./ButtonNext";
 import InputR from "./InputR";
 import InputC from "./InputC";
+import PhotoInput from "./PhotoInput";
 
 const StageThree = () => {
+  const { color } = useContext(stagesContext);
   const [shadows, setShadows] = useState({ s1: false, s2: false, s3: false });
+  const [hasImage, setHasImage] = useState(true);
+  const [imageError, setImageError] = useState(false);
+  const [imageMessage, setImageMessage] = useState("");
 
   useEffect(() => {
-    setTimeout(() => {
-      setShadows((prevShadow) => {
-        return { ...prevShadow, s1: true };
-      });
-    }, 2800);
-    setTimeout(() => {
-      setShadows((prevShadow) => {
-        return { ...prevShadow, s2: true };
-      });
-    }, 3000);
-    setTimeout(() => {
-      setShadows((prevShadow) => {
-        return { ...prevShadow, s3: true };
-      });
-    }, 3200);
-  }, []);
+    if (!hasImage) {
+      setTimeout(() => {
+        setShadows((prevShadow) => {
+          return { ...prevShadow, s1: true };
+        });
+      }, 2800);
+      setTimeout(() => {
+        setShadows((prevShadow) => {
+          return { ...prevShadow, s2: true };
+        });
+      }, 3000);
+      setTimeout(() => {
+        setShadows((prevShadow) => {
+          return { ...prevShadow, s3: true };
+        });
+      }, 3200);
+    }
+  }, [hasImage]);
   const {
     register,
     handleSubmit,
@@ -41,60 +48,101 @@ const StageThree = () => {
     setPlaceholdersApperance,
   } = useContext(stagesContext);
   return (
-    <motion.form
-      autoComplete="off"
-      className="flex flex-col items-center mx-8 "
-      variants={motionvariants.variantStageThree}
-      initial="hidden"
-      animate="visible"
-      exit={{ opacity: 0, display: "none", transition: { duration: 0.2 } }}
-      onSubmit={handleSubmit((data) => {
-        console.log("Form submited!");
-        console.log(data);
-        QRCode.toDataURL(data.codeInput)
-          .then((url) => {
-            console.log(url);
-            setDownload(url);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
+    <>
+      {hasImage ? (
+        <motion.div
+          className="flex flex-col relative mx-8 items-center "
+          initial="hidden"
+          animate="visible"
+          variants={motionvariants.variantStageThree}
+        >
+          <PhotoInput setImageError={setImageError} />
+          {imageError ? (
+            <motion.div
+              className="flex items-center font-bold text-lg justify-center absolute inset-0 rounded-md"
+              style={{
+                color: "#222",
+                backgroundColor: color.color,
+                boxShadow: `0px 10px 24px 0px ${color.color}`,
+              }}
+              onClick={() => {
+                setImageError((val) => !val);
+              }}
+            >
+              <motion.button className="flex absolute text-black top-2 right-2">
+                x
+              </motion.button>
+              <motion.p className="opacity-100">Photo not detected!</motion.p>
+            </motion.div>
+          ) : null}
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            onClick={() => {
+              setHasImage((prev) => !prev);
+            }}
+            style={{ color: color.color }}
+            variants={motionvariants.itemBottom}
+          >
+            I do not
+          </motion.button>
+        </motion.div>
+      ) : (
+        <motion.form
+          autoComplete="off"
+          className="flex flex-col items-center mx-8 "
+          variants={motionvariants.variantStageThree}
+          initial="hidden"
+          animate="visible"
+          exit={{ opacity: 0, display: "none", transition: { duration: 0.2 } }}
+          onSubmit={handleSubmit((data) => {
+            console.log("Form submited!");
+            console.log(data);
+            QRCode.toDataURL(data.codeInput)
+              .then((url) => {
+                console.log(url);
+                setDownload(url);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
 
-        setStage((prevStage) => prevStage + 1);
-      })}
-    >
-      <InputR
-        register={register}
-        shadows={shadows}
-        setShadows={setShadows}
-        placeholdersApperance={placeholdersApperance}
-        setPlaceholdersApperance={setPlaceholdersApperance}
-      />
-      {errors.rowInput && (
-        <span className="text-[#555]">This field is required!</span>
+            setStage((prevStage) => prevStage + 1);
+          })}
+        >
+          <InputR
+            register={register}
+            shadows={shadows}
+            setShadows={setShadows}
+            placeholdersApperance={placeholdersApperance}
+            setPlaceholdersApperance={setPlaceholdersApperance}
+          />
+          {errors.rowInput && (
+            <span className="text-[#555]">This field is required!</span>
+          )}
+          <InputC
+            register={register}
+            shadows={shadows}
+            setShadows={setShadows}
+            placeholdersApperance={placeholdersApperance}
+            setPlaceholdersApperance={setPlaceholdersApperance}
+          />
+          {errors.colInput && (
+            <span className="text-[#555]">This field is required!</span>
+          )}
+          <CodeInput
+            shadows={shadows}
+            setShadows={setShadows}
+            register={register}
+            placeholdersApperance={placeholdersApperance}
+            setPlaceholdersApperance={setPlaceholdersApperance}
+          />
+          {errors.codeInput && (
+            <span className="z-0 text-[#555]">{errors.codeInput.message}</span>
+          )}
+          <ButtonNext setShadow={setShadows} />
+        </motion.form>
       )}
-      <InputC
-        register={register}
-        shadows={shadows}
-        setShadows={setShadows}
-        placeholdersApperance={placeholdersApperance}
-        setPlaceholdersApperance={setPlaceholdersApperance}
-      />
-      {errors.colInput && (
-        <span className="text-[#555]">This field is required!</span>
-      )}
-      <CodeInput
-        shadows={shadows}
-        setShadows={setShadows}
-        register={register}
-        placeholdersApperance={placeholdersApperance}
-        setPlaceholdersApperance={setPlaceholdersApperance}
-      />
-      {errors.codeInput && (
-        <span className="z-0 text-[#555]">{errors.codeInput.message}</span>
-      )}
-      <ButtonNext setShadow={setShadows} />
-    </motion.form>
+    </>
   );
 };
 
